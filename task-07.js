678e460cbb2817dfbd990aec096044db361124ca
velmor99 <volgagaz24100@gmail.com>
2020-03-06 00:29:42 +0200

    fix 7

--- task-07.js
+++ task-07.js
@@ -9,24 +9,10 @@ const isLoginValid = function(login) {
 	}
 	return valid;
 };
-console.log(isLoginValid('Ajax'));
-console.log(isLoginValid('robotGoogles'));
-console.log(isLoginValid('Zod'));
-console.log(isLoginValid('jqueryisextremelyfast'));
 
 const isLoginUnique = function(allLogins, login) {
-	let unique;
-	if (allLogins.join(' ').includes(login)) {
-		unique = false;
-	} else {
-		unique = true;
-	}
-	return unique;
+	return !allLogins.includes(login);
 };
-console.log(isLoginUnique(logins, 'Ajax'));
-console.log(isLoginUnique(logins, 'robotGoogles'));
-console.log(isLoginUnique(logins, 'Zod'));
-console.log(isLoginUnique(logins, 'jqueryisextremelyfast'));
 
 const addLogin = function(allLogins, login) {
 	let add;
@@ -42,9 +28,7 @@ const addLogin = function(allLogins, login) {
 	return add;
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
+
 console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
 console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
 console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
